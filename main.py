be7fa3f0c4bc9ff9dd37a71d795aa094aa042cef
Matthewous <moguch98@mail.ru>
2022-08-07 18:37:38 +0300

    Программа без лога и новой папки и выходных данных

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class VK:
                 photo['name'] = "{}{}{}".format(likes, date, '.jpg')
 
             photos_list.append(photo)
-            pprint(photos_list)
+            # pprint(photos_list)
         return photos_list
 
     def saving_photos(self, photos_list):
@@ -63,7 +63,7 @@ class YandexDisk:
         headers = self.get_headers()
         params = {"url": url, "path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
-        pprint(response.json())
+        # pprint(response.json())
         return response.json()
 
     def delete_photo(self, filename):
@@ -76,23 +76,8 @@ class YandexDisk:
         response.raise_for_status()
         if response.status_code == 201:
             print("File uploded successfully")
-            ya.delete_photo(self, filename = filename)
 
-
-
-if __name__ == '__main__':
-    access_token = 'vk1.a.ZjkyK3CWlLayW6w85jrr_3K8Vy6DE660eVjR0MStPbfBzYZtdaZqWuuSRqWqdTiZZbV2pGC6gtQtEfzYsVIfMY8w2qm7oTsFYzvtB1lrqxXNHWn_JK7hoVVMc84tiFf-4XWMH2_3UHWwirzZsIO9uNAWp0NxRbRh06TQ8f4oX7bSdbgutDmpw4RpzZFkmkmT'
-    user_id = '28357841'
-    vk = VK(access_token, user_id)
-    ya = YandexDisk(token="AQAAAABAMm1eAADLW6b9S6DeTEqPnuTnyrveYOA")
-    # vk.get_profile_photos()
-    # ya.upload_file_to_disk(url, path, path)
-    # vk.get_profile_photos()
-    # vk.saving_photos(vk.get_profile_photos())
-    # ya.upload_file_to_disk()
-    
-    
-
+def function():
     list = vk.get_profile_photos()
     vk.saving_photos(list)
     for photo in list:
@@ -100,11 +85,27 @@ if __name__ == '__main__':
         disk_file_path = photo['name']
         filename = photo['name']
         ya.upload_file_to_disk(url, disk_file_path, filename)
+        ya.delete_photo(filename)
+    print('Programm completed successfully')
+    return
 
 
-    # https://oauth.vk.com/blank.html#access_token=vk1.a.ZjkyK3CWlLayW6w85jrr_3K8Vy6DE660eVjR0MStPbfBzYZtdaZqWuuSRqWqdTiZZbV2pGC6gtQtEfzYsVIfMY8w2qm7oTsFYzvtB1lrqxXNHWn_JK7hoVVMc84tiFf-4XWMH2_3UHWwirzZsIO9uNAWp0NxRbRh06TQ8f4oX7bSdbgutDmpw4RpzZFkmkmT&expires_in=86400&user_id=28357841&state=123456
-    # https://cloud-api.yandex.net/v1/disk/resources/upload?url=https://sun9-45.userapi.com/impf/c626123/v626123841/1ed25/nrgwpEDKROc.jpg?size=498x751&quality=96&sign=dacc5e0e0f0b1eaf47352d0bbe589905&c_uniq_tag=a9O98Tk200T8XsftcbUnREiN2WemHy7ayZXm0-BTqFA&type=album&
-    # access_token = 'vk1.a.ibbE-OL4k0Q-5qS0VyvnzJcpD9tn6IPh1j-fpYgG0WmM9CkGOMGIy12RFkK3nP1zV4dCmzUcVa6-8HxdRdDNHBrdZRi86kFeGfeYU2gisZq3mlaCSxwpfbm5tPISCFhrMg7wCyDjTjZhlxG8O3fC9XvyOJqZUL707CP16jBdztHfkA0D0CzFsRhFcXKujLzc'
-    # url = 'https://sun9-45.userapi.com/impf/c626123/v626123841/1ed25/nrgwpEDKROc.jpg?size=498x751&quality=96&sign=dacc5e0e0f0b1eaf47352d0bbe589905&c_uniq_tag=a9O98Tk200T8XsftcbUnREiN2WemHy7ayZXm0-BTqFA&type=album'
-    # path = 'Photo/nrgwpEDKROc.jpg'
+if __name__ == '__main__':
+    access_token = 'vk1.a.ZjkyK3CWlLayW6w85jrr_3K8Vy6DE660eVjR0MStPbfBzYZtdaZqWuuSRqWqdTiZZbV2pGC6gtQtEfzYsVIfMY8w2qm7oTsFYzvtB1lrqxXNHWn_JK7hoVVMc84tiFf-4XWMH2_3UHWwirzZsIO9uNAWp0NxRbRh06TQ8f4oX7bSdbgutDmpw4RpzZFkmkmT'
+    user_id = '28357841'
+    vk = VK(access_token, user_id)
+    ya = YandexDisk(token="AQAAAABAMm1eAADLW6b9S6DeTEqPnuTnyrveYOA")
+    
+    function()
+
+    # list = vk.get_profile_photos()
+    # vk.saving_photos(list)
+    # for photo in list:
+    #     url = photo['url']
+    #     disk_file_path = photo['name']
+    #     filename = photo['name']
+    #     ya.upload_file_to_disk(url, disk_file_path, filename)
+    #     ya.delete_photo(filename)
+
+
    
